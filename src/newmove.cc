040551739521765090a8ce9e7250d73544fc8d63
Czipperz <czipperz@gmail.com>
2015-08-21 09:11:30 -0700

    Bunch of fixes to movement

--- src/newmove.cc
+++ src/newmove.cc
@@ -7,18 +7,11 @@
 #include "newmove.hh"
 #include "show_message.hh"
 
-typedef std::vector<std::string>* contents;
-
-static long
-    y = 0,x = 0,
-    desired_x;
-static bool waiting_for_desired;
-
-static int to_visual(const std::string& cont, int x) {
+int to_visual(const std::string& cont, int x) {
     int til = 0,
-        xx = 0;
+        xx = -1;
     for(std::string::const_iterator i = cont.begin();
-                                    i < cont.begin() + x; i++) {
+                                    i <= cont.begin() + x; i++) {
         if(*i == '\t') {
             xx += TAB_SIZE() - 1 - til;
             if(xx > x) return xx;
@@ -32,29 +25,29 @@ static int to_visual(const std::string& cont, int x) {
     return xx;
 }
 
-static void redrawyx(contents contents) {
-    move(y,to_visual((*contents)[y],x));
+void redrawyx(contents& contents) {
+    move(contents.y,to_visual((*contents.cont)[contents.y],contents.x));
 }
 
-void mvline(contents contents, unsigned long line) {
-    mvrel(contents, line - y, 0);
+void mvline(contents& contents, unsigned long line) {
+    mvrel(contents, line - contents.y, 0);
 }
-void mv(contents contents, unsigned long _y, unsigned long _x) {
-    mvrel(contents, _y - y, _x - x);
+void mv(contents& contents, unsigned long _y, unsigned long _x) {
+    mvrel(contents, _y - contents.y, _x - contents.x);
 }
 
-void mvrel(contents contents, long y, long x) {
+void mvrel(contents& contents, long y, long x) {
     if(y < 0) mvu(contents,-y);
     else      mvd(contents, y);
     if(x < 0) mvb(contents,-x);
     else      mvf(contents, x);
 }
 
-void mvcol(contents contents, unsigned long col) {
-    unsigned int len = (*contents)[y].length();
+void mvcol(contents& contents, unsigned long col) {
+    unsigned int len = (*contents.cont)[contents.y].length();
     if(len >= col) {
-        x = col;
-        waiting_for_desired = false;
+        contents.x = col;
+        contents.waiting_for_desired = false;
     } else {
         show_message((std::string("Can't move to column: ")
                       + int_to_str(col)).c_str());
@@ -62,57 +55,60 @@ void mvcol(contents contents, unsigned long col) {
     redrawyx(contents);
 }
 
-void mvsot(contents contents) {
+void mvsot(contents& contents) {
     mvsol(contents);
-    const std::string& str = (*contents)[y];
+    const std::string& str = (*contents.cont)[contents.y];
     for(unsigned int i = 0; i < str.length(); i++)
         if(str[i] == ' ' || str[i] == '\t') mvf(contents);
         else break;
 }
 
-void mveol(contents contents) {
-    mvcol(contents,(*contents)[y].length() - 1);
+void mveol(contents& contents) {
+    mvcol(contents,(*contents.cont)[contents.y].length() - 1);
 }
-void mvsol(contents contents) { mvcol(contents,0); }
+void mvsol(contents& contents) { mvcol(contents,0); }
 
-void mvsop(contents contents) {
-    y = 0;
-    x = 0;
+void mvsop(contents& contents) {
+    contents.y = 0;
+    contents.x = 0;
+    contents.waiting_for_desired = false;
     redrawyx(contents);
 }
-void mveop(contents contents) {
-    y = contents->size();
-    x = 0;
+void mveop(contents& contents) {
+    contents.y = contents.cont->size();
+    contents.x = 0;
+    contents.waiting_for_desired = false;
     redrawyx(contents);
 }
 
-void mvd(contents contents, long times) {
-    std::vector<std::string> cont = *contents;
-    if(y + times < 0 || y + times >= cont.size()) {
+void mvd(contents& contents, long times) {
+    if(contents.y + times < 0 || contents.y + times >= contents.cont->size()) {
         show_message("Can't move to that location (start/end of buffer)");
         return;
     }
-    y += times;
-    unsigned int len = cont[y].length();
-    if(waiting_for_desired) {
-        if(x >= len) {
-            x = len - 1;
-        } else if(desired_x >= x) {
-            waiting_for_desired = false;
-            x = desired_x;
+    contents.y += times;
+    unsigned int len = (*contents.cont)[contents.y].length();
+    if(contents.waiting_for_desired) {
+        if(contents.x >= len) {
+            contents.x = len - 1;
+        } else if(contents.desired_x > contents.x
+                  && contents.desired_x < len) {
+            // x  desired  len
+            contents.x = contents.desired_x;
+            contents.waiting_for_desired = false;
         } else {
-            
+            // x  len  desired
+            contents.x = len - 1;
         }
-    } else {
-        if(len < x) {
-            waiting_for_desired = true;
-            desired_x = x;
-            x = len;
-        }
-    }
+    } else if(len <= contents.x) {
+        contents.waiting_for_desired = true;
+        contents.desired_x = contents.x;
+        contents.x = len - 1;
+    } //x doesn't need to adjusted otherwise
+    contents.x = contents.x >= 0 ? contents.x : 0;
     redrawyx(contents);
 }
-void mvu(contents contents, long times) { mvd(contents,-times); }
+void mvu(contents& contents, long times) { mvd(contents,-times); }
 
 
 static bool isDeliminator(char ch) {
@@ -120,47 +116,46 @@ static bool isDeliminator(char ch) {
         if(DELIMINATORS()[i] == ch) return true;
     return false;
 }
-void mvfw(contents contents,unsigned long words) {
+void mvfw(contents& contents,unsigned long words) {
 }
-void mvbw(contents contents,unsigned long words) {
+void mvbw(contents& contents,unsigned long words) {
 }
 
 
-void mvf(contents contents, unsigned long times) {
-    std::vector<std::string> cont = *contents;
-    unsigned long newx = x + times;
-    while(cont[y].length() + 1 <= newx) {
-        try {
-            y++;
-            newx -= cont.at(y).length() + 1;
-        } catch(...) {
-            break;
+inline static unsigned int fixLen(unsigned int len) {
+    return len ? len : 1;
+}
+void mvf(contents& contents, unsigned long times) {
+    long newx = contents.x + times;
+    try {
+        while(fixLen(contents.cont->at(contents.y).length()) <= newx) {
+            newx -= fixLen((*contents.cont)[contents.y].length());
+            contents.y++;
         }
-    }
-    if(y >= cont.size()) y = cont.size()-1;
-    x = newx;
-    show_message((std::string("for (x,y): (") + int_to_str(x)
-                  + "," + int_to_str(y) + ")").c_str());
+    } catch(...) { }
+    if(contents.y >= contents.cont->size()) contents.y = contents.cont->size()-1;
+    if(contents.x < 0) contents.x = 0;
+    else      contents.x = newx;
+    show_message((std::string("for (x,y): (") + int_to_str(contents.x)
+                  + "," + int_to_str(contents.y) + ")").c_str());
+    contents.waiting_for_desired = false;
     redrawyx(contents);
 }
-void mvb(contents contents, unsigned long times) {
-    std::vector<std::string> cont = *contents;
-    long newx = x - times;
+void mvb(contents& contents, unsigned long times) {
+    long newx = contents.x - times;
     while(newx < 0) {
         try {
-            y--;
-            newx += cont.at(y).length();
+            contents.y--;
+            newx += fixLen(contents.cont->at(contents.y).length());
         } catch(...) {
             break;
         }
     }
-    if(y < 0) y = 0;
-    if(x < 0) {
-        x = 0;
-    } else {
-        x = newx;
-    }
-    show_message((std::string("back (x,y): (") + int_to_str(x)
-                  + "," + int_to_str(y) + ")").c_str());
+    if(contents.y < 0) contents.y = 0;
+    if(contents.x < 0) contents.x = 0;
+    else      contents.x = newx;
+    show_message((std::string("back (x,y): (") + int_to_str(contents.x)
+                  + "," + int_to_str(contents.y) + ")").c_str());
+    contents.waiting_for_desired = false;
     redrawyx(contents);
 }
