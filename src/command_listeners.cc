030805aaeb6d6d27835f87f025d0d0bf42879c55
Czipperz <czipperz@gmail.com>
2015-08-25 00:16:29 -0700

    Fixed command_listeners: mem leak, spaciate, error message

--- src/command_listeners.cc
+++ src/command_listeners.cc
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <ncurses.h>
 #include <map>
-#include <string>
+#include <ncurses.h>
 #include <stdlib.h>
+#include <string>
 #include <vector>
 
 #include "configuration.hh"
 #include "int_to_str.hh"
 #include "key_aliases.hh"
-#include "show_message.hh"
 #include "prompt.hh"
+#include "show_message.hh"
 
 static std::map<std::string,
                 void (*)(const std::vector<std::string>&)>
@@ -25,17 +25,36 @@ void command_executor() {
     std::vector<std::string>* args = spaciate(pr);
     std::string name = (*args)[0];
     args->erase(args->begin());
-    if(commandMap.count(name)) commandMap[name](*args);
-    else show_message((std::string("Unrecognized command: ") + name +
-                       " with args: " + inter_space(args)).c_str());
+    if(commandMap.count(name)) {
+        commandMap[name](*args);
+    } else if(args->size() != 0) {
+        show_message((std::string("Unrecognized command: ") + name +
+                      " with args: " + inter_space(args)).c_str());
+    } else {
+        show_message((std::string("Unrecognized command: ") + name +
+                      " with null args.").c_str());
+    }
+    delete args;
 }
+
+
+#define _check \
+    if(cur.length() != 0) {     \
+        spaced->push_back(cur); \
+        cur = ""; \
+    }
 
 static std::vector<std::string>* spaciate(const std::string& tospace) {
     std::vector<std::string>* spaced = new std::vector<std::string>();
     std::string cur;
     for(unsigned int i = 0; i < tospace.length(); i++) {
-        if(spaced[i] == ' ' || spaced[i] == '\t') {
-            
+        if(tospace[i] == ' ' || tospace[i] == '\t') {
+            _check;
+        } else {
+            cur += tospace[i];
         }
     }
+    _check;
+    return spaced;
 }
+#undef _check
