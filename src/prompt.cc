8c878d72642d5a66e9f1a9d9ad08b359efd87703
Czipperz <czipperz@gmail.com>
2015-08-25 00:14:35 -0700

    Fixed prompt functions

--- src/prompt.cc
+++ src/prompt.cc
@@ -1,10 +1,11 @@
+#include <iostream>
 #include <ncurses.h>
 #include <stdlib.h>
 #include <string>
-#include <iostream>
+
 #include "key_aliases.hh"
 
-std::string prompt_for_test(const std::string& message, unsigned int y) {
+std::string prompt(const std::string& message, unsigned int y) {
     std::string text;
     int b_y,b_x,
         x = 0,
@@ -17,38 +18,32 @@ std::string prompt_for_test(const std::string& message, unsigned int y) {
     move(y,x);
 
     while(true) {
-        std::string str;
-        char c;
-        while(true) {
-            switch(getch()) {
-            case ERR:
-                std::cout << "ERROR character: " << ERR;
-                endwin();
-                exit(2);
-                break;
-            case _escape:
-                // timeout(0);//if waiting char then get
-                // char ch = getch();
-                // if(ch == -1) {
-                move(b_y,b_x);
-                return "";
-                // }
-                // timeout(-1);
-                break;
-            default:
-                move(y, ++xtrack);
-                addch(c);
-                str += c;
-                break;
-            }
+        char c = getch();
+        switch(c) {
+        case ERR:
+            std::cout << "ERROR character: " << ERR;
+            endwin();
+            exit(2);
+            break;
+        case _escape:
+            move(b_y,b_x);
+            return "";
+        case '\n':
+            move(b_y,b_x);
+            return text;
+        default:
+            move(y, ++xtrack);
+            addch(c);
+            text += c;
+            break;
         }
     }
 
     return text;
 }
 
-std::string prompt_for_test(const std::string& message) {
+std::string prompt(const std::string& message) {
     int y,x;
     getmaxyx(stdscr,y,x);
-    return prompt_for_test(message,y-1);
+    return prompt(message,y-1);
 }
