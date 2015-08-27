033fc732ddf4f04232c5e6bdc61692400f168878
Czipperz <czipperz@gmail.com>
2015-08-27 02:40:24 -0700

    Don't run ``tryinit'' because how RAII works

--- src/hooks.cc
+++ src/hooks.cc
@@ -3,23 +3,13 @@
 #include "hooks.hh"
 
 static std::map<Hook::Hook, std::vector<void (*)()> > hooks;
-static bool init(false);
-static void tryinit() {
-    if(!init) {
-        hooks[Hook::MOVE] = std::vector<void (*)()>();
-        hooks[Hook::SAVE] = std::vector<void (*)()>();
-        hooks[Hook::EDIT] = std::vector<void (*)()>();
-    }
-}
 
 void proc_hook(Hook::Hook hook) {
-    tryinit();
     std::vector<void (*)()> x = hooks[hook];
     for(int i = 0; i < x.size(); i++)
         x[i]();
 }
 
 void add_hook(Hook::Hook hook, void (*val)()) {
-    tryinit();
     hooks[hook].push_back(val);
 }
