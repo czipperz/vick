4daf46921ff05a0f585d85ad7c320ba1d7c9cb51
Czipperz <czipperz@gmail.com>
2015-08-26 21:58:35 -0700

    ``refresh'' doesn't die if given empty file

--- src/file_contents.cc
+++ src/file_contents.cc
@@ -16,6 +16,7 @@ contents* get_contents() { return cont; }
 
 void print_contents(const contents* contents) {
     clear();
+    if(contents->cont->size() == 0) return;
     int b_y,b_x,
         y = 0;
     getyx(stdscr,b_y,b_x);
