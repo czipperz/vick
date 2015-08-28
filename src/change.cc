77a9c9eeb3c5f920687e0271c143e35cdae77bc9
Czipperz <czipperz@gmail.com>
2015-08-28 13:53:08 -0700

    Fixed implementation and added constructor that takes a raw std::function

--- src/change.cc
+++ src/change.cc
@@ -1,11 +1,20 @@
 #include "change.hh"
 
 change::change(void (*handle)(std::vector<std::string>&))
+    : handle(std::function
+               <void(std::vector<std::string>&)>
+                 (handle)) { }
+
+change::change(std::function<void(std::vector<std::string>&)> handle)
     : handle(handle) { }
-change change::operator>>(change& other) {
-    return change([&,other,this] (std::vector<std::string>& contents)
-                  {
-                      other(contents);
-                      this(contents);
-                  });
+
+change change::operator>>(const change& other) const {
+    std::function < void(std::vector <std::string>&) > func(
+        [&,other,this] (std::vector<std::string>& contents)
+        { other(contents);   (*this)(contents); });
+    return change(func);
+}
+
+void change::operator()(std::vector<std::string>& contents) const {
+    handle(contents);
 }
--- src/change.hh
+++ src/change.hh
@@ -1,16 +1,22 @@
 #ifndef HEADER_GUARD_CHANGE_H
 #define HEADER_GUARD_CHANGE_H
 
+#include <functional>
 #include <string>
 #include <vector>
 
 class change {
     private:
-    void (*const handle)(std::vector<std::string>&);
+    const std::function<void(std::vector<std::string>&)> handle;
+
     public:
     change(void (*)(std::vector<std::string>&));
-    change operator>>(change&); //then
-    void operator()(std::vector<std::string>& contents);
+    change(std::function<void(std::vector<std::string>&)>);
+
+    //combines two changes together
+    //mvd >> mvsot >> mvf   would past the first character on the next line
+    change operator>>(const change&) const;
+    void operator()(std::vector<std::string>& contents) const;
 };
 
 #endif
