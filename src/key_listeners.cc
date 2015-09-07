a5b6e57d7a44df19b338e0f22106bc0b2a19d8ca
Czipperz <czipperz@gmail.com>
2015-09-06 20:46:05 -0700

    Fix ``loop'' as that refresh entire screen every keypress

--- src/key_listeners.cc
+++ src/key_listeners.cc
@@ -9,17 +9,24 @@
 #include "key_listeners.hh"
 #include "show_message.hh"
 #include "hooks.hh"
+#include "to_str.hh"
 
 void loop() {
     add_listeners();
     add_hook(Hook::REFRESH,hook_show_message);
 
     while(true) {
-        char ch(getch());
+        char ch = getch();
         clear_message();
         if(!get_contents()->m(ch)) {
             show_message((std::string("Didn't recognize key press: ")
                           + ch).c_str());
+        } else {
+            show_message((std::string("x,y: (")
+                          + int_to_str(get_contents()->x) + ','
+                          + int_to_str(get_contents()->y) + ')').c_str());
         }
+        get_contents()->refreshmaxyx();
+        print_contents(get_contents());
     }
 }
