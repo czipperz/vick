6a77ff59300929907c4ee812f2b8ef03dd8a70b4
Czipperz <czipperz@gmail.com>
2015-09-08 14:04:01 -0700

    Define a crap ton more key_aliases

--- src/key_aliases.hh
+++ src/key_aliases.hh
@@ -1,8 +1,51 @@
 #ifndef HEADER_GUARD_KEY_ALIASES_H
 #define HEADER_GUARD_KEY_ALIASES_H
 
-#define _control_a 1
-#define _control_e 5
-#define _escape    27
+#define _control_a            1
+#define _control_b            2
+#define _control_c            3
+#define _control_d            4
+#define _control_e            5
+#define _control_f            6
+#define _control_g            7
+#define _control_h            8
+#define _control_i            9
+#define _control_j            10
+#define _control_k            11
+#define _control_l            12
+#define _control_m            _control_j /*wat*/
+#define _control_n            14
+#define _control_o            15
+#define _control_p            16
+#define _control_q            17
+#define _control_r            18
+#define _control_s            19
+#define _control_t            20
+#define _control_u            21
+#define _control_v            22
+#define _control_w            23
+#define _control_x            24
+#define _control_y            25
+#define _control_z            26
+
+#define _control_3            _escape
+#define _control_4            _control_backslash
+#define _control_5            _control_close_square
+#define _control_6            _control_circonflex
+#define _control_7            _control_underscore
+#define _control_8            _backspace
+
+#define _escape               27
+#define _control_open_square  27
+#define _control_backslash    28
+#define _control_close_square 29
+#define _control_circonflex   30
+#define _control_tilde        _control_circonflex
+#define _control_shift_six    _control_circonflex
+#define _control_underscore   31
+#define _backspace            127
+#define _resize              -102
+
+#define _control_forwardslash _control_underscore
 
 #endif
