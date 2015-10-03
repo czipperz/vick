286a2cdba33c0a5887d602787956fd9260ddc1a0
Czipperz <czipperz@gmail.com>
2015-10-03 16:16:09 -0700

    Make names indentation consistent in prefix

--- src/prefix.hh
+++ src/prefix.hh
@@ -10,17 +10,15 @@ class prefix {
     private:
     // use this for documentation
     char prech;
-
-    std::map < char, std::function < void ( contents&,
-                                            boost::optional<int> ) > > map;
+    std::map < char, std::function < void ( contents&, boost::optional<int> ) > > map;
 
     public:
     prefix(char);
-    void push_back(char, std::function < void ( contents&,
-                                                boost::optional<int> ) > );
-    void operator() ( contents& cont, boost::optional<int> );
-    operator std::function < void ( contents&,
-                                    boost::optional<int> ) > ();
+
+    void push_back(char, std::function < void
+                                  ( contents&, boost::optional<int> ) > );
+    void operator ()              ( contents&, boost::optional<int> );
+    operator std::function < void ( contents&, boost::optional<int> ) > ();
 };
 
 #endif
