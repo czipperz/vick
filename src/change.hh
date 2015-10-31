2aa2a2bb5e791ff0a209238393b5974e3be541c4
Czipperz <czipperz@gmail.com>
2015-10-31 07:27:39 -0700

    Wrap examples in ``change.hh`` documentation in code blocks so format properly.

--- src/change.hh
+++ src/change.hh
@@ -63,12 +63,14 @@ struct change {
  * \brief Undoes the change on top of the stack, or pops off another
  * if called again
  *
+ * \code{.cpp}
  * ----------
  * | ------ |
  * | before |
  * | after  |
  * | ------ |
  * ----------
+ * \endcode
  */
 boost::optional<std::shared_ptr<change> >
 undo_change(contents&, boost::optional<int> = boost::none);
@@ -77,12 +79,14 @@ undo_change(contents&, boost::optional<int> = boost::none);
  * \brief Redoes the change on top of the stack, or climbs back on it 
  * Deletes anything after current
  *
+ * \code{.cpp}
  * ----------
  * | ------ |
  * | after  |
  * | before |
  * | ------ |
  * ----------
+ * \endcode
  */
 boost::optional<std::shared_ptr<change> >
 redo_change(contents&, boost::optional<int> = boost::none);
@@ -91,11 +95,13 @@ redo_change(contents&, boost::optional<int> = boost::none);
  * \brief Reapplies the current change on the current position
  * Deletes anything after current
  *
+ * \code{.cpp}
  * -----------------------
  * | current (reapplied) |
  * | current             |
  * | ------------------- |
  * -----------------------
+ * \endcode
  */
 boost::optional<std::shared_ptr<change> >
 reapply_change(contents&, boost::optional<int> = 0);
