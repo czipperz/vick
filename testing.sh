b68849f2642c0359ddbabc1f9864f0da2f493d0d
Czipperz <czipperz@gmail.com>
2016-03-24 11:37:15 -0500

    Move cabal update things to ./testing.sh

--- testing.sh
+++ testing.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+cabal update
+cabal install shake
+
 shakedir=.shake
 mkdir -p $shakedir
 ghc --make Build.hs -rtsopts -with-rtsopts=-I0 -outputdir=$shakedir \
     -o make -DIS_IN_TESTING=True
+
 ./make test
