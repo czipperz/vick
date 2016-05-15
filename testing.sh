7a3684520c6db3be96575cf740037c442e538bfa
Czipperz <czipperz@gmail.com>
2016-05-15 16:47:31 -0700

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cabal update
-cabal install shake
 
-shakedir=.shake
-mkdir -p $shakedir
-ghc --make Build.hs -rtsopts -with-rtsopts=-I0 -outputdir=$shakedir \
-    -o make -DIS_IN_TESTING=True
+git clone https://github.com/czipperz/vick-build
 
-./make test
+cd vick-build
+./configure
+make
+cd ..
+
+./configure
+make
